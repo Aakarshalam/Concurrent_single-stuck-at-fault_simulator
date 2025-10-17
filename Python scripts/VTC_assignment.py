2c61728696520592c8571df61cfc0eb7eca14af8
nani? <126398356+Aakarshalam@users.noreply.github.com>
2025-10-17 10:08:32 +0530

    Update VTC_assignment.py

--- Python scripts/VTC_assignment.py
+++ Python scripts/VTC_assignment.py
@@ -12,8 +12,8 @@ for small structural Verilog circuits (AND/OR/NOT/DFF subset).
 Usage:
   Clone the repository and run the following commands in Command Line / Terminal to generate reports.
   python generate_vectors.py
-  python faultsim.py --verilog mux2to1.v --top mux2to1 --vectors vectors.csv --out report.txt 
-  python faultsim.py --verilog seq_ckt.v --top seq_ckt --vectors vectors_seq.csv --out report.txt
+  python VTC_assignment.py --verilog mux2to1.v --top mux2to1 --vectors vectors.csv --out report.txt 
+  python VTC_assignment.py --verilog seq_ckt.v --top seq_ckt --vectors vectors_seq.csv --out report.txt
 """
 
 import argparse
